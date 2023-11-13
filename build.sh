c5dfaa1995aa03874e599c1a8fc8833ce31cbb2b
nickchomey <88559987+nickchomey@users.noreply.github.com>
2023-11-13 09:01:37 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 
 # Install build dependencies
 sudo apt-get update
-sudo apt-get install -y build-essential libssl-dev libcurl4-openssl-dev gnupg
+sudo apt-get install -y build-essential libssl-dev libcurl4-openssl-dev
 
 # Download and extract the latest version of curl
 latest_curl_version=$(curl -s https://curl.se/download/ | grep -o 'curl-[0-9.]*.tar.gz' | head -n 1)
 curl -LO https://curl.se/download/$latest_curl_version
 curl -LO https://curl.se/download/$latest_curl_version.asc
 
-tar -xzvf $latest_curl_version
+tar -xzf $latest_curl_version
 curl_folder_name=$(basename $latest_curl_version .tar.gz)
 cd $curl_folder_name
 
